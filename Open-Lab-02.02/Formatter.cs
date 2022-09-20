15db1a0ad865e6fcc1f2e16c144400dcb8ed37aa
samuelmalik <samuelmalik9@gmail.com>
2022-09-20 18:54:30 +0200

    added function rounding numbers to 2 decimals and printing them separated with spaces

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+
+                Console.Write(Math.Round(numbers[i], 2) + " ");
+            }
+            Console.Write(Math.Round(numbers[numbers.Length - 1], 2));
+
         }
     }
 }
